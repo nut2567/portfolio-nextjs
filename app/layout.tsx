05caa8910d4312496f41fd2d5e57afacc6d5cf97
DESKTOP-JRPF1OL\gooka_9jgac6y <nutmakchine.n@gmail.com>
2024-10-23 17:47:27 +0700

    add image edit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { ThemeSwitcher } from "@/components/theme-switcher";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
@@ -10,6 +9,7 @@ import Image from "next/image";
 import "./globals.css";
 import SideNavigation from "@/components/layouts/SideNavigation";
 import Header from "@/components/layouts/header";
+import Headerbar from "@/components/layouts/Headerbar";
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
@@ -40,42 +40,11 @@ export default function RootLayout({
           {/* {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />} */}
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex gap-5 font-semibold justify-between items-center p-3 px-5 text-sm">
-                  <div><p>Portfolio</p><p>Nutthawat Witdumring</p></div>
 
-                  <div><div className="flex justify-center w-full"> powered by
-                    <div className=" ml-2">
-                    <Image
-                        className="dark:invert"
-                        src="https://nextjs.org/icons/next.svg"
-                        alt="Next.js logo"
-                        width={90}
-                        height={18}
-                        priority
-                      />
-                    </div>
-                  </div></div>
-                </div>
-              </nav>
+              <Headerbar />
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
-              </div>
-
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
+              </div>              
             </div>
           </main>
         </ThemeProvider>
--- components/layouts/SideNavigation.tsx
+++ components/layouts/SideNavigation.tsx
@@ -49,7 +49,19 @@ export default function SideNavigation({ }) {
       <div className={`z-50 fixed inset-0 w-12 bg-slate-800 text-white flex flex-col transform  
       duration-600 transition-all`}   >
         <div className="p-4 text-lg font-bold text-center">
-          <div className="border shadow-xl border-indigo-500/50"></div></div>
+          <div className="border shadow-xl border-indigo-500/50">
+            <div className="mx-5">
+              <Image
+                className=""
+                src="/images/nut.jpg"
+                alt="me"
+                width={90} // กำหนดความกว้างของรูปภาพ
+                height={0} // ปล่อยค่า height เป็น auto โดย Next.js จะจัดการเอง
+                style={{ height: 'auto' }} // กำหนดให้ความสูงปรับตามสัดส่วนของภาพ
+                priority // ใช้ให้ Next.js โหลดภาพนี้เป็น priority
+              />
+            </div>
+          </div></div>
 
         <nav className="flex flex-col space-y-2">
           <button><Link href="/"><div
@@ -64,19 +76,26 @@ export default function SideNavigation({ }) {
           <button><Link href="/addproduct"><div
             className="flex items-center p-2 hover:bg-gray-700 rounded justify-center"
           ><div className="flex">
-              <i className="material-icons items-center flex ">stars</i>
+              <i className="material-icons items-center flex ">account_tree</i>
 
             </div>
           </div></Link></button>
 
 
           <button><Link href="/profile">
-          <div className="flex items-center p-2 hover:bg-gray-700 rounded justify-center"
-          ><div className="flex">
-              <i className="material-icons items-center flex ">person</i>
+            <div className="flex items-center p-2 hover:bg-gray-700 rounded justify-center"
+            ><div className="flex">
+                <i className="material-icons items-center flex ">code_off</i>
 
-            </div>
-          </div></Link></button>
+              </div>
+            </div></Link></button>
+          <button><Link href="/profile">
+            <div className="flex items-center p-2 hover:bg-gray-700 rounded justify-center"
+            ><div className="flex">
+                <i className="material-icons items-center flex ">perm_contact_calendar</i>
+
+              </div>
+            </div></Link></button>
 
           <button><Link href="/dashboard">
             <div className="flex items-center p-2 hover:bg-gray-700 rounded justify-center"
