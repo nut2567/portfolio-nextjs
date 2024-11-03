9cac504ef4a2581cbca5bba405da2f7f420457d3
nut2567 <nutmakchine.n@gmail.com>
2024-11-04 05:23:26 +0700

    Type error: Type '{ searchParams: Message; }' does not satisfy the constraint 'PageProps'.

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -6,7 +6,11 @@ import { Label } from '@/components/ui/label'
 import Link from 'next/link'
 import { SmtpMessage } from '../smtp-message'
 
-export default function Signup({ searchParams }: { searchParams: Message }) {
+export default function Signup({
+  searchParams,
+}: {
+  searchParams: Message | any
+}) {
   if (!searchParams.message) {
     return (
       <div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
